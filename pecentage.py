b4ade16f1bfb02141e046b5fe8f9d2f1c4124df9
Hardik-Gangwani <hardikgangwani2010@gmail.com>
2025-09-21 15:16:26 +0530

    Add files via upload

---
+++ pecentage.py
@@ -0,0 +1,9 @@
+a = int(input("total no of working days:"))
+b = int(input("total no of days present:"))
+c=(b/a)*100 
+print("percentage of days present is:",c)
+if c>=75:
+    print("allowed to sit in exam") 
+else:
+    print("not allowed to sit in exam") 
+    1
